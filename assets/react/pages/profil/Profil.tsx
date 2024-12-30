3cd20a417ca871a8d5843432f3f8c15d47fc282d
cmadoka2003 <148872395+cmadoka2003@users.noreply.github.com>
2024-12-30 09:21:17 +0100

    V2

--- assets/react/pages/profil/Profil.tsx
+++ assets/react/pages/profil/Profil.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { getMethod, postMethod } from '../../services/axiosInstance';
-import { set } from 'date-fns';
 
 type Company = {
     id: number;
