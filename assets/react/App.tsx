d304e5f68bfbd7a35fa75229da6078af23c35d0e
cmadoka2003 <148872395+cmadoka2003@users.noreply.github.com>
2025-01-27 10:48:49 +0100

    Suppression de la page UserProfil.tsx

--- assets/react/App.tsx
+++ assets/react/App.tsx
@@ -5,7 +5,6 @@ import Home from './pages/home/Home';
 import VolsPage from './pages/vols/page';
 import UserProvider from './context/UserContext';
 import Favorite from './pages/profil/Favorite';
-import UserProfil from './pages/profil/UserProfil';
 import Trips from './pages/trips/Trips';
 import Profil from './pages/profil/Profil';
 import Parametres from './pages/profil/parametres';
@@ -27,7 +26,6 @@ function App() {
                 <Route path='/profil' element={<Profil />} />
                 <Route path='/favorite' element={<Favorite />} />
                 <Route path="/trips" element={<Trips />} />
-                <Route path='/UserProfil' element={<UserProfil />} />
                 <Route path='/profil/parametres' element={<Parametres />}/>
                 <Route path="/profil/preferences" element={<Preferences/>}/>
                 <Route path="/profil/voyageurs" element={<Voyageur/>}/>
